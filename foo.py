e6fb1ee42c29b40a2e05fe479ca6c4ff74af8964
jaseg <s@jaseg.de>
2012-06-07 15:00:00 +0200

    Fixed the PWM (mostly). It appears there is some nonlinearity somewhere around value 80, but I am not sure about that.

--- foo.py
+++ foo.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import serial
 import sys
+import time
 ser = serial.Serial("/dev/ttyACM0", 57600)
 
-while True:
-    print ser.read()
+#val = 0;
+#while True:
+#    ser.write('a')
+#    ser.write(chr(0))
+#    ser.write(chr(val))
+#    ser.write('\n')
+#    val+=1
+#    val%=256
+#    time.sleep(0.25);
 
 while True:
     val = int(sys.stdin.readline())
--- pwm.c
+++ pwm.c
@@ -9,36 +9,30 @@ void pwm_loop(){}
 
 void pwm_setup(){
     //s/w "BCM"(<== "Binary Code Modulation") timer setup
-    TCCR0A |= _BV(WGM00)|_BV(WGM01);
-    TCCR0B |= _BV(CS01)|_BV(CS00)|_BV(WGM02);
-    TIMSK0 |= _BV(TOIE0);
+    TCCR0A |= _BV(WGM01);
+    TCCR0B |= _BV(CS02);
+    TIMSK0 |= _BV(OCIE0A);
     OCR0A = 1;
     pwm_output_setup();
-    //FIXME debug stuff
-    DDRB |= 0x20;
 }
 
-//Software PWM stuff
-//Called every 256 cpu clks (i.e should not get overly long)
-ISR(TIMER0_OVF_vect){
+ISR(TIMER0_COMPA_vect){
+    pwm_unset_outputs();
     pwm_cycle<<=1;
     if(!pwm_cycle){
         pwm_cycle = 1;
     }
     uint8_t Q = 0;
-    if(pwm_cycle&0x80)
-        PORTB ^= 0x20;
-    pwm_unset_outputs();
-    Q |= !!(pwm_val[0] & pwm_cycle);
+    Q |= (pwm_val[0] & pwm_cycle)?1:0;
     Q |= (pwm_val[1] & pwm_cycle)?2:0;
     Q |= (pwm_val[2] & pwm_cycle)?4:0;
     Q |= (pwm_val[3] & pwm_cycle)?8:0;
-    /*Q |= (pwm_val[4] & pwm_cycle)?16:0;
+    Q |= (pwm_val[4] & pwm_cycle)?16:0;
     Q |= (pwm_val[5] & pwm_cycle)?32:0;
     Q |= (pwm_val[6] & pwm_cycle)?64:0;
     Q |= (pwm_val[7] & pwm_cycle)?128:0;
-    */
     pwm_set_outputs(Q);
+    TCNT0 = 0;
     OCR0A = pwm_cycle;
 }
 
