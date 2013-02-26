2f4a86a8b41cedb2ea24f8c46ea18beb241fd49d
jaseg <s@jaseg.de>
2013-02-26 02:52:40 +0100

    Refined the magick of the ganglion a bit. Unittest not checket yet.

--- pylibcerebrum/ganglion.py
+++ pylibcerebrum/ganglion.py
@@ -21,13 +21,13 @@ from pylibcerebrum.NotifyList import NotifyList
 class TimeoutException(Exception):
 	pass
 
-class Ganglion:
+class Ganglion(object):
 	"""Proxy class for calling remote methods on hardware connected through a serial port using the Cerebrum protocol"""
 
 	# NOTE: the device config is *not* the stuff from the config "dev" section but
 	#read from the device. It can also be found in that [devicename].config.json
 	#file created by the code generator
-	def __init__(self, device=None, baudrate=115200, config=None, ser=None):
+	def __init__(self, device=None, baudrate=115200, jsonconfig=None, ser=None):
 		"""Ganglion constructor
 
 		Keyword arguments:
@@ -36,21 +36,21 @@ class Ganglion:
 		The other keyword arguments are for internal use only.
 
 		"""
-		#FIXME HACK to et the initialization go smooth despite the __*__ special functions and "config" not yet being set
-		self._config = None
+		# get a config
 		if ser is None:
-			assert(config is None)
+			assert(jsonconfig is None)
 			s = serial.Serial(port=device, baudrate=baudrate, timeout=1)
 			#Trust me, without the following two lines it *wont* *work*. Fuck serial ports.
 			s.setXonXoff(True)
 			s.setXonXoff(False)
 			s.setDTR(True)
 			s.setDTR(False)
-			self._opened_ser = self._ser = s
+			object.__setattr__(self, '_opened_ser', s)
+			object.__setattr__(self, '_ser', s)
 			i=0
 			while True:
 				try:
-					self._config = self._read_config()
+					jsonconfig = self._read_config()
 					time.sleep(0.1)
 					break
 				except TimeoutException as e:
@@ -62,8 +62,20 @@ class Ganglion:
 					raise serial.serialutil.SerialException('Could not connect, giving up after 20 tries')
 		else:
 			assert(device is None)
-			self._config = config
-			self._ser = ser
+			object.__setattr__(self, '_ser', ser)
+		# populate the object
+		object.__setattr__(self, 'members', {})
+		for name, member in jsonconfig.get('members', {}).items():
+			self.members[name] = Ganglion(jsonconfig=member, ser=self._ser)
+		object.__setattr__(self, 'properties', {})
+		for name, prop in jsonconfig.get('properties', {}).items():
+			self.properties[name] = (prop['id'], prop['fmt'], prop.get('access', 'rw'))
+		object.__setattr__(self, 'functions', {})
+		for name, func in jsonconfig.get('functions', {}).items():
+			def proxy_method(*args):
+				return self._callfunc(func["id"], fun.get("args", ""), args, func.get("returns", ""))
+			self.functions[name] = func
+		object.__setattr__(self, 'config', { k: v for k,v in jsonconfig.items() if not k in ['members', 'properties', 'functions'] })
 	
 	def __del__(self):
 		self.close()
@@ -91,27 +103,6 @@ class Ganglion:
 		"""Construct an iterator to iterate over *all* (direct or not) child nodes of this node."""
 		return GanglionIter(self)
 
-	@property
-	def members(self):
-		"""Return a list of child node names of this node."""
-		if "members" in self._config:
-			return list(self._config["members"].keys())
-		return []
-
-	@property
-	def properties(self):
-		"""Return a list of property names of this node."""
-		if "properties" in self._config:
-			return list(self._config["properties"].keys())
-		return []
-
-	@property
-	def functions(self):
-		"""Return a list of function names of this node."""
-		if "functions" in self._config:
-			return list(self._config["functions"].keys())
-		return []
-
 	def _my_ser_read(self, n):
 		"""Read n bytes from the serial device and raise a TimeoutException in case of a timeout."""
 		data = self._ser.read(n)
@@ -129,22 +120,25 @@ class Ganglion:
 			return json.JSONDecoder().decode(str(lzma.decompress(cbytes[1:]), "utf-8"))
 		else:
 			return json.JSONDecoder().decode(str(cbytes, "utf-8"))
-
+	
 	def _callfunc(self, fid, argsfmt, args, retfmt):
 		"""Call a function on the device by id, directly passing argument/return format parameters."""
+		# Make a list out of the arguments if they are none
 		if not (isinstance(args, tuple) or isinstance(args, list)):
 			args = [args]
+		# Send the encoded data
 		cmd = b'\\#' + struct.pack("<HH", fid, struct.calcsize(argsfmt)) + struct.pack(argsfmt, *args)
 		self._ser.write(cmd)
-		#payload length
+		# payload length
 		(clen,) = struct.unpack(">H", self._my_ser_read(2))
-		#payload data
+		# payload data
 		cbytes = self._my_ser_read(clen)
 		if clen != struct.calcsize(retfmt):
-			#CAUTION! This error is thrown not because the user supplied a wrong value but because the device answered in an unexpected manner.
-			#FIXME raise an error here or let the whole operation just fail in the following struct.unpack?
+			# CAUTION! This error is thrown not because the user supplied a wrong value but because the device answered in an unexpected manner.
+			# FIXME raise an error here or let the whole operation just fail in the following struct.unpack?
 			raise AttributeError("Device response format problem: Length mismatch: {} != {}".format(clen, struct.calcsize(retfmt)))
 		rv = struct.unpack(retfmt, cbytes)
+		# Try to interpret the return value in a useful manner
 		if len(rv) == 0:
 			return None
 		elif len(rv) == 1:
@@ -154,20 +148,21 @@ class Ganglion:
 
 	def __dir__(self):
 		"""Get a list of all attributes of this object. This includes virtual Cerebrum stuff like members, properties and functions."""
-		return self.members + self.properties + self.functions + list(self.__dict__.keys())
-
+		return list(self.members.keys()) + list(self.properties.keys()) + list(self.functions.keys()) + list(self.__dict__.keys())
+	
+	# Only now add the setattr magic method so it does not interfere with the above code
 	def __setattr__(self, name, value):
 		"""Magic method to set an attribute. This one even handles remote Cerebrum properties."""
-		if name is not "_config": #Guard against all too infinite recursion
-			#check if the name is a known property
-			if self._config and "properties" in self._config and name in self._config["properties"]:
-				#call the property's Cerebrum setter function
-				var = self._config["properties"][name]
-				if not "w" in var.get("access", "rw"):
-					raise TypeError("{} is a read-only property".format(name))
-				return self._callfunc(var["id"]+1, var["fmt"], value, "")
+		#check if the name is a known property
+		if name in self.properties:
+			#call the property's Cerebrum setter function
+			varid, varfmt, access = self.properties[name]
+			if not "w" in access:
+				raise TypeError("{} is a read-only property".format(name))
+			return self._callfunc(varid+1, varfmt, value, "")
 		#if the above code falls through, do a normal __dict__ lookup.
 		self.__dict__[name] = value
+
 
 	def __getattr__(self, name):
 		"""Magic method to get an attribute of this object, considering Cerebrum members, properties and functions.
@@ -177,29 +172,22 @@ class Ganglion:
 		Between a member, a property and a function of the same name the member will be preferred over the property and the property will be preferred over the function. If you should manage to make device have such colliding names, consider using _callfunc(...) directly.
 
 		"""
-		if not self._config: #Guard against all too infinite recursion
-			raise AttributeError(name)
+		if name in self.members:
+			return self.members[name]
 
-		if "members" in self._config and name in self._config["members"]:
-			g = Ganglion(config=self._config["members"][name], ser=self._ser)
-			self.__dict__[name] = g
-			return g
-
-		if "properties" in self._config and name in self._config["properties"]:
-			var = self._config["properties"][name]
+		if name in self.properties:
 			def cb(newx):
 				self.__setattr__(name, newx)
-			rv = self._callfunc(var["id"], "", (), var["fmt"])
+			varid, varfmt, access = self.properties[name]
+			rv = self._callfunc(varid, "", (), varfmt)
+			# If the return value is a list, construct an auto-updating thingy from it.
 			if isinstance(rv, list):
 				return NotifyList(rv, callbacks=[cb])
 			else:
 				return rv
 
-		if "functions" in self._config and	name in self._config["functions"]:
-			fun = self._config["functions"][name]
-			def proxy_method(*args):
-				return self._callfunc(fun["id"], fun.get("args", ""), args, fun.get("returns", ""))
-			return proxy_method
+		if name in self.functions:
+			return self.functions[name]
 
 		#If all of the above falls through...
 		raise AttributeError(name)
