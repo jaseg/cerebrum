9122a8c3884c4d2eb57bb5ab0e6b812815639cd9
jaseg <s@jaseg.de>
2013-06-13 00:54:57 +0200

    Added c-beam to mainhall-rgb

--- tools/mainhall-rgb.py
+++ tools/mainhall-rgb.py
@@ -6,7 +6,8 @@ import json
 import requests
 from pylibcerebrum.serial_mux import SerialMux
 
-BASE_URI	='http://10.0.1.43/dmxacl/json/'
+BASE_URI	= 'http://10.0.1.43/dmxacl/json/'
+CBEAM		= 'http://10.0.1.27:4254/rpc/'
 PORT		= '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A700fmkX-if00-port0'
 BAUDRATE	= 57600
 GAMMA		= 2.5
@@ -41,26 +42,38 @@ def inc_color(r,g,b):
 		v['blue']	= xform(v['blue'], b)
 	requests.post(BASE_URI, data=json.dumps({'method': 'lightSync.push', 'params': [state], 'id': 0}))
 
+def sendstate(schnur, state):
+	requests.post(CBEAM, data=json.dumps({'method': 'barschnur', 'params': [schnur, state], 'id': 0}))
+
 oldstate = None
 sr, sg, sb = False, False, False
 while 1:
 	if g.schnurlinks.state:
 		if sr:
+			sendstate('links', False)
 			inc_color(1,0,0)
 		sr = False
 	else:
+		if not sr:
+			sendstate('links', True)
 		sr = True
 	if g.schnurmitte.state:
 		if sg:
+			sendstate('mitte', False)
 			inc_color(0,1,0)
 		sg = False
 	else:
+		if not sg:
+			sendstate('mitte', True)
 		sg = True
 	if g.schnurrechts.state:
 		if sb:
+			sendstate('rechts', False)
 			inc_color(0,0,1)
 		sb = False
 	else:
+		if not sb:
+			sendstate('rechts', True)
 		sb = True
 	time.sleep(0.1)
 
