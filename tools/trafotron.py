113b4692c1c569a2f00216fd2dd291b7f6b4a1cd
jaseg <jaseg@jaseg.net>
2013-10-26 21:39:34 +0200

    Added trafotron bar status indicator control

--- tools/trafotron.py
+++ tools/trafotron.py
@@ -23,14 +23,48 @@ print('opening first device')
 g = s.open(0)
 print('initializing device')
 print(dir(g))
+#bar status outputs
+for io in [g.digital3, g.digital5, g.digital6, g.digital9, g.digital10, g.digital11]:
+	io.direction = 1
+	io.pwm_enabled = 1
 print('starting event loop')
+
+#HACK ctrl-c ctrl-p -ed from barstatus.py
+barstatus = 'closed'
+lastchange = time.time()
+def animate():
+	global barstatus, lastchange
+	while True:
+		lookup = barstatus
+		if time.time() - lastchange < 180:
+			if barstatus == 'open':
+				lookup = 'opened'
+			elif barstatus == 'closed':
+				lookup = 'lastcall'
+		l1, r1 = {'open': ((10, 255, 10), (128, 128, 128)),
+			'opened': ((10, 128, 255), (128, 128, 128)),
+			'closed': ((128, 128, 128), (255, 4, 4)),
+		      'lastcall': ((10, 255, 10), (128, 128, 128))}.get(lookup)
+		l2, r2 = {'open': ((10, 255, 10), (128, 128, 128)),
+			'opened': ((10, 255, 10), (128, 128, 128)),
+			'closed': ((128, 128, 128), (255, 4, 4)),
+		      'lastcall': ((10, 255, 10), (255, 255, 10))}.get(lookup)
+		(g.digital3.pwm, g.digital5.pwm, g.digital6.pwm), (g.digital9.pwm, g.digital10.pwm, g.digital11.pwm) = l1, r1
+		time.sleep(0.33)
+		(g.digital3.pwm, g.digital5.pwm, g.digital6.pwm), (g.digital9.pwm, g.digital10.pwm, g.digital11.pwm) = l2, r2
+		time.sleep(0.66)
+
+animator = Thread(target=animate)
+animator.daemon = False
+animator.start()
+
 
 def sendstate(value):
 	print('SENDING', value)
 	requests.post(CBEAM, data=json.dumps({'method': 'trafotron', 'params': [value], 'id': 0}))
 
-# Set Arduino digital pin 2 to output
-g.digital2.state = 1
+# Enable pull-up on Arduino analog pin 1
+g.analog1.state = 1
 oldval = -2*SEND_THRESHOLD
 oldbarstate = None
 newbarstate = None
@@ -39,12 +73,19 @@ while True:
 	if abs(val-oldval) > SEND_THRESHOLD:
 		oldval = val
 		sendstate(int(val))
-	if g.digital2.state:
+	if g.analog1.state:
 		newbarstate = 'closed'
 	else:
 		newbarstate = 'open'
 	if newbarstate != oldbarstate:
-		requests.post(CBEAM, data=json.dumps({'method': 'barstatus', 'params': [newbarstate], 'id': 0}))
-		requests.post(HYPERBLAST, data=json.dumps({'method': 'barstatus', 'params': [newbarstate], 'id': 0}))
 		oldbarstate = newbarstate
+
+		#comm with animation thread
+		global barstatus
+		barstatus = status
+		global lastchange
+		lastchange = time.time()
+
+		requests.post(HYPERBLAST, data=json.dumps({'method': 'barstatus', 'params': [newbarstate], 'id': 0}))
+		requests.post(CBEAM, data=json.dumps({'method': 'barstatus', 'params': [newbarstate], 'id': 0}))
 
